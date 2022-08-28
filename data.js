32295f2ba7ea0e75a9182dad1f397d6a190f0656
Ctc8 <97801841+Ctc8@users.noreply.github.com>
2022-08-28 12:47:35 -0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,51 @@
+export const creatorDetails = {
+    accountHandle: '@Ctc8',
+    creatorName: 'Collin Chuang',
+    tagline: 'insert tagline here',
+    profilePic: 'blankAvatar.png',
+}
+
+export const socialLinks = [
+    {
+        url: 'https://github.com/Ctc8',
+        iconName: 'github',
+        active: true,
+    },
+
+    {
+        url: 'https://www.youtube.com/channel/UC3IxCzA5QOjAE4uDwes6UKw',
+        iconName: 'youtube',
+        active: true,
+    },
+
+    {
+        url: 'https://www.linkedin.com/in/collin-chuang-828076227/',
+        iconName: 'linkedin',
+        active: true,
+    },
+
+    {
+        url: 'https://open.spotify.com/user/asapkid12?si=7201834e4320401d',
+        iconName: 'spotify',
+        active: true,
+    },
+
+];
+
+export const whereNextLinks = [
+    {
+        url: '',
+        text: 'My Developer Portfolio',
+        active: true,
+        },
+    {
+        url: '',
+        text: 'About Me',
+        active: true,
+        },
+    {
+        url: '',
+        text: 'Website',
+        active: true,
+        },    
+]
